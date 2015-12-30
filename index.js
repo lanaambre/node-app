c3889a55c27f3dc9ba826097a3b39dbce9ae873d
Antoine Demailly <antoine.demailly@gmx.fr>
2015-12-30 20:12:55 +0100

    Add socket.io and mongoose -> index.js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 // Config
-var config = require('./app/config');
+var config        = require('./app/config');
 
+// App
 var express       = require('express');
 var app           = express();
 
 var server        = require('http').Server(app);
+
+// Socket.io
+var io            = require('socket.io')(server);
+
+// MongoDB
+var mongoose      = require('mongoose');
+mongoose.connect('mongodb://localhost/' + config.db.name);
 
 server.listen(8888, function() {
   console.log('Listening on *:8888');
