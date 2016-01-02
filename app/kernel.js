1dcf4a4c36a4694d85116f65e06c42b9870413dd
Antoine Demailly <antoine.demailly@gmx.fr>
2016-01-02 15:49:13 +0100

    Add template gestion -> app/kernel.js

--- app/kernel.js
+++ app/kernel.js
@@ -1,23 +1,30 @@
 var kernel = function() {
-  // Config
+  // Get Config
   var config        = require('./config');
 
-  // App
+  // Define App & Server
   var express       = require('express');
   var app           = express();
-
   var server        = require('http').Server(app);
 
-  // Socket.io
+  // Define Socket.io
   var io            = require('socket.io')(server);
 
-  // MongoDB
+  // Define MongoDB
   var mongoose      = require('mongoose');
   mongoose.connect('mongodb://localhost/' + config.db.name);
+
+  // Define static folder
+  app.use(express.static('public'));
+
+  // Define view engine
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
 
   // Require Controllers
   require('./controllers/ExampleController')(app);
 
+  // Launch server
   server.listen(8888, function() {
     console.log('Listening on *:8888');
   });
